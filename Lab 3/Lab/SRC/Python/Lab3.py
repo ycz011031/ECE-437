8a57f191b57b0e7cb88e47fb56c9ebc5ceacba4c
unknown <yz69@illinois.edu>
2024-09-09 19:25:59 -0500

    Lab3 Python

---
+++ Lab 3/Lab/SRC/Python/Lab3.py
@@ -0,0 +1,39 @@
+import time   # time related library
+import sys,os    # system related library
+ok_sdk_loc = "C:\\Program Files\\Opal Kelly\\FrontPanelUSB\\API\\Python\\x64"
+ok_dll_loc = "C:\\Program Files\\Opal Kelly\\FrontPanelUSB\\API\\lib\\x64"
+sys.path.append(ok_sdk_loc)   # add the path of the OK library
+os.add_dll_directory(ok_dll_loc)
+import ok     # OpalKelly library
+
+dev = ok.okCFrontPanel()  # define a device for FrontPanel communication
+SerialStatus=dev.OpenBySerial("")      # open USB communication with the OK board
+ConfigStatus=dev.ConfigureFPGA("../FPGA/bit/lab2_example.bit"); # Configure the FPGA with this bit file
+print("----------------------------------------------------")
+
+
+if SerialStatus == 0:
+    print ("FrontPanel host interface was successfully initialized.")
+else:    
+    print ("FrontPanel host interface not detected. The error code number is:" + str(int(SerialStatus)))
+    print("Exiting the program.")
+    sys.exit ()
+
+if ConfigStatus == 0:
+    print ("Your bit file is successfully loaded in the FPGA.")
+else:
+    print ("Your bit file did not load. The error code number is:" + str(int(ConfigStatus)))
+    print ("Exiting the progam.")
+    sys.exit ()
+print("----------------------------------------------------")
+print("----------------------------------------------------")
+
+while(True):
+    dev.SetWireInValue(0x00,0)
+    dev.UpdateWireIns()
+    input("Press Enter to Generate Pedestrian button press")
+    dev.SetWireInValue(0x00,1)
+    dev.Update_WireIns()
+
+
+    
